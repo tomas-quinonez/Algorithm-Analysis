17859196d6c42e3d00262c94f5c87248e8e6ea06
tomyl <tomyl@DESKTOP-TT044NO>
2019-09-06 18:16:17 -0300

    se cambió el método de ordenamiento de los buckets. De de quicksort a burbuja

--- src/Tp1/TPO1.java
+++ src/Tp1/TPO1.java
@@ -6,6 +6,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TPO1 {
+
+    public static void burbuja(int[] a) {
+        for (int i = 0; i <= a.length - 2; i++) {
+            for (int j = 0; j <= a.length - 2 - i; j++) {
+                if (sumaDigitos(a[j + 1]) < sumaDigitos(a[j])) {
+                    int aux = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = aux;
+
+                }
+                if (sumaDigitos(a[j + 1]) == sumaDigitos(a[j])) {
+                    if (a[j + 1] < a[j]) {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+
+                }
+            }
+        }
+    }
+
     private static int[] obtenerValorMinMax(int[] arr) {
         int[] aux = new int[2];
         if (arr.length >= 2) {
@@ -25,34 +47,33 @@ public class TPO1 {
 
         return aux;
     }
-    
+
     public static int sumaDigitos(int num) {
-        int temp=0;
-        while (num>1) {
-            temp = temp+num % 10;
-            num= num/10;
-        }        
-            temp = temp+num % 10;
+        int temp = 0;
+        while (num > 1) {
+            temp = temp + num % 10;
+            num = num / 10;
+        }
+        temp = temp + num % 10;
         return temp;
     }
 
-    public static int[] arregloSuma(int[] arr){
-        int[] temp= new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            temp[i]=sumaDigitos(arr[i]);
+    public static int[] arregloSuma(int[] arr) {
+        int[] temp = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = sumaDigitos(arr[i]);
         }
         return temp;
-    } 
+    }
 
     public static void bucketsort(int[] ar) {
-        int[] temp=arregloSuma(ar);
+        int[] temp = arregloSuma(ar);
         int[] valores = obtenerValorMinMax(temp);
         int minVal = valores[0];
         int maxVal = valores[1];
 
-
         int n = ar.length; // n es el numero de elementos
-        int m = maxVal - minVal+1; // m es el rango de valores
+        int m = maxVal - minVal + 1; // m es el rango de valores
         int cantBuckets = m / n + 1;
         List<List<Integer>> buckets = new ArrayList<>(cantBuckets);
         for (int i = 0; i < cantBuckets; i++) {
@@ -62,8 +83,8 @@ public class TPO1 {
         // Ubicar cada elemento en un bucket
         for (int i = 0; i < n; i++) {
             int bi = (temp[i] - minVal) / m;
-            System.out.println(buckets.size()+" n: "+n+" bi: "+bi);
-            if(bi==buckets.size()){
+            System.out.println(buckets.size() + " n: " + n + " bi: " + bi);
+            if (bi == buckets.size()) {
                 bi--;
             }
             List<Integer> bucket = buckets.get(bi);
@@ -71,14 +92,14 @@ public class TPO1 {
         }
 
         // Ordenar los buckets y mezclarlos en un solo arreglo
-        for (int bi = 0, j=0; bi < cantBuckets; bi++) {
+        for (int bi = 0, j = 0; bi < cantBuckets; bi++) {
             List<Integer> bucket = buckets.get(bi);
             if (bucket != null) {
                 //Collections.sort(bucket);
                 Object[] lista = bucket.toArray();
-                int[] listaInt = Arrays.stream(lista).mapToInt(o -> (int)o).toArray();
-                if(listaInt.length>1){
-                    quicksort(listaInt);
+                int[] listaInt = Arrays.stream(lista).mapToInt(o -> (int) o).toArray();
+                if (listaInt.length > 1) {
+                    burbuja(listaInt);
                 }
                 for (int k = 0; k < listaInt.length; k++) {
                     ar[j++] = listaInt[k];
@@ -86,15 +107,12 @@ public class TPO1 {
             }
         }
     }
-    
+
     public static void main(String[] args) {
-        int[] arr = {14,30,55,83,24,52,11};
-        int[] arr2 = {14,30,55,83,24,52,11};
+        int[] arr = {50, 14, 30, 55, 83, 24, 5, 52, 11};
         bucketsort(arr);
-        quicksort(arr2);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
-            System.out.println(arr2[i]);
         }
     }
 }
